fd3eed19bda3c971a426eaf5608be2d4e6b557a7
ktavarez2021 <44203242+ktavarez2021@users.noreply.github.com>
2019-01-15 10:23:44 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,27 @@
+//Spell #1
+
+$(".button").click(function(){
+    $(".picture").fadeToggle();
+ 
+});
+
+
+
+//Spell #2
+
+$("#hat").click(function(){
+    $("#bunny").slideToggle();
+ 
+});
+
+
+
+//Spell #3
+
+$(".stop").click(function(){
+    $(".netflix").toggle();
+ 
+});
+
+
+
